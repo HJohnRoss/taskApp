32533e053ef9fe145d4a6df46fb8a3bd042bb492
FJohnHRoss <rossjohn936@gmail.com>
2023-05-25 15:49:13 -0700

    scss setup

---
+++ client/src/components/topNav/TopNav.tsx
@@ -0,0 +1,8 @@
+
+
+const TopNav = () => {
+  <>
+  </>
+}
+
+export default TopNav;
---
+++ client/src/veiws/Dashboard.tsx
@@ -0,0 +1,10 @@
+
+const dashboard = () => {
+  return (
+    <>
+      <h1>Hello World</h1>
+    </>
+  )
+}
+
+export default dashboard
