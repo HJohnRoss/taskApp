737dd224acd6681bf6ad8415d2a4f90373608773
Brandon DeBenedictis <brandonchasedebenedictis@gmail.com>
2023-06-06 21:40:50 -0700

    changes

--- client/src/components/boards/BoardHolder.jsx
+++ client/src/components/boards/BoardHolder.jsx
@@ -1,18 +1,19 @@
 import CalendarBoard from './CalendarBoard/CalendarBoard'
 import TaskBoard from './TaskBoard/TaskBoard'
+import BoardService from "../services/BoardService";
+import {useParams} from "react-router-dom"
 
 function BoardHolder() {
 
-  // const { id } = useParams();
+  const { id } = useParams();
 
-  // let service = BoardService.getOne(id)
-  // console.log(service)
+  let service = BoardService.getOne(id)
+  console.log(service)
 
   return (
     <div className='board'>
       <CalendarBoard />
-      {/* <CalendarBoard/> */}
-      <TaskBoard />
+
     </div>
   )
 }
