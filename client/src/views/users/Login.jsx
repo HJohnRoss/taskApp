58b6314e893a0aea1e557af2445156001f7bc468
FJohnHRoss <rossjohn936@gmail.com>
2023-06-19 12:12:00 -0700

    login/reg update

--- client/src/views/users/Login.jsx
+++ client/src/views/users/Login.jsx
@@ -2,20 +2,32 @@ import TopNav from "../../components/navbars/TopNav"
 import { useState, useEffect } from "react"
 
 const Login = () => {
-  const [userName, setUserName] = useState("");
+  const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
   const [submit, setSubmit] = useState(false);
 
+  const [errors, setErrors] = useState([]);
+
   useEffect(() => {
     if (password.length < 8) {
       setSubmit(false)
-    } else if (userName.length < 3) {
+    } else if (username.length < 3) {
       setSubmit(false)
     } else {
       setSubmit(true)
     }
-  }, [password, userName])
+
+    let arr = []
+    if (username.length < 3 && username.length > 1) {
+      arr.push("Username must be at least 3 characters long")
+    }
+    if (password.length < 8 && password.length > 1) {
+      arr.push("Password must be at least 8 characters long")
+    }
+    setErrors(arr)
+    console.log(errors)
+  }, [password, username])
 
   return (
     <div className="center">
@@ -32,8 +44,19 @@ const Login = () => {
 
           <form className="login__container--form" action="" method="post">
             <h2 className="login__container--form--header">Login</h2>
+
+            <div className="login__container--errors">
+              {
+                errors.map((error, i) => {
+                  return(
+                    <p key={i}>{error}</p>
+                  )
+                })
+              }
+              </div>
+
             <label className="login__container--form--label" htmlFor="userName">User Name:</label>
-            <input type="text" className="login__container--form--input" onChange={e => setUserName(e.target.value)} value={userName} />
+            <input type="text" className="login__container--form--input" onChange={e => setUsername(e.target.value)} value={username} />
 
             <label className="login__container--form--label" htmlFor="password">Password:</label>
             <input type="password" className="login__container--form--input" onChange={e => setPassword(e.target.value)} value={password} />
--- client/src/views/users/Register.jsx
+++ client/src/views/users/Register.jsx
@@ -2,12 +2,14 @@ import TopNav from "../../components/navbars/TopNav"
 import { useState, useEffect } from "react"
 
 const Register = () => {
-  const [userName, setUserName] = useState("");
+  const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmed, setConfirmed] = useState("");
 
   const [submit, setSubmit] = useState(false);
+
+  const [errors, setErrors] = useState([]);
 
   useEffect(() => {
     if (password !== confirmed) {
@@ -16,12 +18,27 @@ const Register = () => {
       setSubmit(false)
     } else if (!email.includes("@") || !email.includes(".com")) {
       setSubmit(false)
-    } else if (userName.length < 3) {
+    } else if (username.length < 3) {
       setSubmit(false)
     } else {
       setSubmit(true)
     }
-  }, [confirmed, password, email, userName])
+
+    let arr = []
+    if (username.length < 3 && username.length > 1) {
+      arr.push("Username must be at least 3 characters long")
+    }
+    if (!email.includes("@") && email.length > 1 || !email.includes(".com") && email.length > 1) {
+      arr.push("Must be a valid email")
+    }
+    if (password.length < 8 && password.length > 1) {
+      arr.push("Password must be at least 8 characters long")
+    } else if (password !== confirmed) {
+      arr.push("Password and confirmed password must match")
+    }
+    setErrors(arr)
+    console.log(errors)
+  }, [confirmed, password, email, username])
 
   return (
     <div className="center">
@@ -32,14 +49,22 @@ const Register = () => {
         <div className="login__container">
 
 
-          <div className="login__container--errors">
-
-          </div>
 
           <form className="login__container--form" action="" method="post">
             <h2 className="login__container--form--header">Register</h2>
-            <label className="login__container--form--label" htmlFor="userName">User Name:</label>
-            <input type="text" className="login__container--form--input" onChange={e => setUserName(e.target.value)} value={userName} />
+
+            <div className="login__container--errors">
+              {
+                errors.map((error, i) => {
+                  return(
+                    <p key={i}>{error}</p>
+                  )
+                })
+              }
+
+            </div>
+            <label className="login__container--form--label" htmlFor="userName">Username:</label>
+            <input type="text" className="login__container--form--input" onChange={e => setUsername(e.target.value)} value={username} />
 
             <label className="login__container--form--label" htmlFor="email">Email:</label>
             <input type="text" className="login__container--form--input" onChange={e => setEmail(e.target.value)} value={email} />
